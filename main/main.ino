21a80e3519a3583eb537b7b4fc972c6008358f43
Jonatas Pirene <jonatasr.2000@gmail.com>
2022-11-16 19:43:37 -0300

    Atuadores e Sistema de Hora
    Definido as funções dos atuadores e as portas
    
    Sistema de Hora Ajustado retornando dataVal

--- main/main.ino
+++ main/main.ino
@@ -7,14 +7,13 @@
 // #include "sntp.h"
 // #include "dht.h" //INCLUSÃO DE BIBLIOTECA Umidade/Temperatura
 
-
 // Wifi
 // #define ssid "Vitor"
 // #define password "Bv082930"
 // #define ssid "WIFI UNIFEOB"
 // #define password ""
-#define ssid "LAPTOPIOT"
-#define password "senha123"
+#define ssid "Toninho"
+#define password "clone9270droid"
 
 // FireBase
 #define FIREBASE_HOST "https://unifeob-iot-2022-default-rtdb.firebaseio.com/"
@@ -22,46 +21,44 @@
 FirebaseData bd;
 
 // Componentes
-#define recebeUmidade 33
-#define ledDesligado 14
-#define ledLigado 13
+#define motor 25
+#define fazenda 26
+#define escola 32
+#define canteiro1 33
+#define recebeUmidade 34
+
 #define serialTime 115200
 
 // Variáveis Time
 const char* ntpServer1 = "pool.ntp.org";
 const char* ntpServer2 = "time.nist.gov";
-const long  gmtOffset_sec = -5400;
-const int   daylightOffset_sec = -5400;
-
-const char* time_zone = "CET-1CEST,M3.5.0,M10.5.0/3";  // Regra de fuso horário para Europa/Roma, incluindo regras de ajuste de luz do dia (opcional)
-
-
+const long  gmtOffset_sec = -10800;
+const int   daylightOffset_sec = -10800;
 
 void setup() {
   
   Serial.begin(serialTime);
   sntp_set_time_sync_notification_cb( timeavailable );
-   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
   WiFi.begin(ssid, password);
-  pinMode(ledDesligado, OUTPUT);
-  pinMode(ledLigado, OUTPUT);
+  
+  pinMode(motor, OUTPUT);
+  pinMode(fazenda, OUTPUT);
+  pinMode(escola, OUTPUT);
+  pinMode(canteiro1, OUTPUT);
+  pinMode(recebeUmidade, INPUT);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    
-  }
-  
-  Serial.println("");
-  Serial.println("Wifi Conectado!!!");
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  
 }
 
 void loop() {
 
   // float DHTtemperatura = DHT.temperature; // O VALOR DE UMIDADE MEDIDO 
   // float DHTumidade = DHT.humidity; // O VALOR DE UMIDADE MEDIDO
+  if(WiFi.status() != WL_CONNECTED)
+  {
+   conectarWifi(); 
+  }
+
   
   float umidade = analogRead(recebeUmidade);  
   // Serial.print(umidade);
@@ -80,57 +77,126 @@ void loop() {
 
         if (umidade < 3600) {
 
-          Firebase.setString(bd, "/escola/canteiro1/sl1", "1");
+          Firebase.setString(bd, "/escola/canteiro1/isOpen", "1");
           // Firebase.pushString(bd, "/canteiro2/historicoUmidade", umidade);// maoir de 4000
 
         } else {
 
           // Firebase.pushString(bd, "/canteiro2/historicoUmidade", umidade);//
-          Firebase.setString(bd, "/escola/canteiro1/sl1", "0");
+          Firebase.setString(bd, "/escola/canteiro1/isOpen", "0");
         }
         
       }
     }
   }
 
-  if (Firebase.get(bd, "/escola/canteiro1/sl1")) {
+  if (Firebase.get(bd, "/escola/canteiro1/isOpen")) {
     if (bd.dataType() == "string") {
       String RL1 = bd.stringData();
       if (RL1 == "0") {
         // as ações
         // Firebase.pushString(bd, "/escola/canteiro1/ultimoStatusUmidade", umidade);  //antes de desligar
-        Firebase.setString(bd, "/escola/canteiro1/statusSl1", "Desligado");
-
-        salvaData();
-        digitalWrite(ledDesligado, HIGH);
-        digitalWrite(ledLigado, LOW);
-
+        Firebase.setString(bd, "/escola/canteiro1/status", "Fechado");
+        digitalWrite(canteiro1, HIGH);
       } else if (RL1 == "1") {
         // as ações
-        Firebase.setString(bd, "/escola/canteiro1/statusSl1", "Ligado");
-
-        salvaData();
-        digitalWrite(ledDesligado, LOW);
-        digitalWrite(ledLigado, HIGH);
+        Firebase.setString(bd, "/escola/canteiro1/status", "Aberto");
+        digitalWrite(canteiro1, LOW);
       }
       
     }
     
   }
 
-}
+  if (Firebase.get(bd, "/escola/isOpen")) {
+    if (bd.dataType() == "string") {
+      String RL1 = bd.stringData();
+      if (RL1 == "0") {
+        // as ações
+        // Firebase.pushString(bd, "/escola/canteiro1/ultimoStatusUmidade", umidade);  //antes de desligar
+        Firebase.setString(bd, "/escola/status", "Fechado");
+        digitalWrite(escola, HIGH);
+      } else if (RL1 == "1") {
+        // as ações
+        Firebase.setString(bd, "/escola/status", "Aberto");
+        digitalWrite(escola, LOW);
+      }
+      
+    }
+    
+  }
 
-void salvaData()
+  if (Firebase.get(bd, "/fazenda/isOpen")) {
+    if (bd.dataType() == "string") {
+      String RL1 = bd.stringData();
+      if (RL1 == "0") {
+        // as ações
+        // Firebase.pushString(bd, "/escola/canteiro1/ultimoStatusUmidade", umidade);  //antes de desligar
+        Firebase.setString(bd, "/fazenda/status", "Fechado");
+        digitalWrite(fazenda, HIGH);
+      } else if (RL1 == "1") {
+        // as ações
+        Firebase.setString(bd, "/fazenda/status", "Aberto");
+        digitalWrite(fazenda, LOW);
+      }
+      
+    }
+    
+  }
+
+  if (Firebase.get(bd, "/motor/isOn")) {
+    if (bd.dataType() == "string") {
+      String RL1 = bd.stringData();
+      if (RL1 == "0") {
+        // as ações
+        // Firebase.pushString(bd, "/escola/canteiro1/ultimoStatusUmidade", umidade);  //antes de desligar
+        Firebase.setString(bd, "/motor/status", "Desligado");
+        digitalWrite(motor, HIGH);
+      } else if (RL1 == "1") {
+        // as ações
+        Firebase.setString(bd, "/motor/status", "Ligado");
+        digitalWrite(motor, LOW);
+      }
+      
+    }
+    
+  }
+}
+void conectarWifi()
+{
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    
+  }
+  
+  Serial.println("");
+  Serial.println("Wifi Conectado!!!");
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+String obterData()
 {
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
     Serial.println("Não há tempo disponivel (ainda)");
-    return;
+    return "";
   }
 
-  String dataVal = (&timeinfo, "%Y/%m/%d");
-  String horaVal = (&timeinfo, "%H:%M:%S");
+//https://cplusplus.com/reference/ctime/tm/
+//tm_sec   int    seconds after the minute   0-61*
+//tm_min   int    minutes after the hour     0-59
+//tm_hour  int    hours since midnight       0-23
+//tm_mday  int    day of the month           1-31
+//tm_mon   int    months since January       0-11
+//tm_year  int    years since 1900
+//tm_wday  int    days since Sunday          0-6
+//tm_yday  int    days since January 1       0-365
+//tm_isdst int    Daylight Saving Time flag   
+
+  String dataVal =  String(timeinfo.tm_mday) + "/" + String(timeinfo.tm_mon+1) + "/" + String(timeinfo.tm_year + 1900) + 
+  " " + String(timeinfo.tm_hour) +":"+ String(timeinfo.tm_min) +":"+ String(timeinfo.tm_sec) ;
   Serial.println(dataVal);
+  return dataVal;
 }
 
 // Função de retorno de chamada (é chamado quando o tempo se ajusta via NTP)
