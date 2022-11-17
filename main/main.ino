644a8d23375eeb27c468701ee3450eaca1914767
Jonatas Pirene <jonatasr.2000@gmail.com>
2022-11-17 17:47:05 -0300

    sensor de fluxo

--- main/main.ino
+++ main/main.ino
@@ -23,6 +23,7 @@ FirebaseData bd;
 // Componentes
 #define motor 25
 #define fazenda 26
+#define recebeFluxo 27
 #define escola 32
 #define canteiro1 33
 #define recebeUmidade 34
@@ -30,12 +31,21 @@ FirebaseData bd;
 #define serialTime 115200
 
 // Variáveis Time
-const char* ntpServer1 = "pool.ntp.org";
-const char* ntpServer2 = "time.nist.gov";
+//const char* ntpServer1 = "pool.ntp.org";
+//const char* ntpServer2 = "time.nist.gov";
+const char* ntpServer1 = "a.st1.ntp.br";
+const char* ntpServer2 = "b.st1.ntp.br";
 const long  gmtOffset_sec = -10800;
 const int   daylightOffset_sec = -10800;
 String data = "";
+
+//Variáveis Umidade
 String caminhoUmidade = ""; 
+
+//Variáveis Fluxo
+double flowRate;    //Este é o valor que pretende-se calcular
+volatile int count; //Este número precisa ser setado como volátil para garantir que ele seja atualizado corretamente durante o processo de interrupção
+String caminhoFluxo = "";
 
 void setup() {
   
@@ -43,12 +53,18 @@ void setup() {
   sntp_set_time_sync_notification_cb( timeavailable );
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
   WiFi.begin(ssid, password);
-  
+
+  //Atuadores
   pinMode(motor, OUTPUT);
   pinMode(fazenda, OUTPUT);
   pinMode(escola, OUTPUT);
   pinMode(canteiro1, OUTPUT);
+
+  //Sensores
   pinMode(recebeUmidade, INPUT);
+  pinMode(recebeFluxo, INPUT); //Seta o pino de entrada
+  attachInterrupt(digitalPinToInterrupt(recebeFluxo), Flow, RISING);  //Configura o pino do Esp(como interruptor) e define para rodar a função "Flow"
+
 }
 
 void loop() {
@@ -62,8 +78,24 @@ void loop() {
 
     //date = obterData();
     //Serial.println(data);
-  
-  
+
+  count = 0;      // Reseta o contador para iniciarmos a contagem em 0 novamente
+  interrupts();   //Habilita a interrupção do pino
+  delay (1000);   //Espera 1 segundo
+  noInterrupts(); //Desabilita o interrupção do pino
+
+   //Cálculos matemáticos
+  flowRate = (count * 2.25);        //Conta os pulsos no último segundo e multiplica por 2,25mL, que é a vazão de cada pulso
+  flowRate = flowRate * 60;         //Converte segundos em minutos, tornando a unidade de medida mL/min
+  flowRate = flowRate / 1000;       //Converte mL em litros, tornando a unidade de medida L/min
+   if(count > 0)
+   {
+     Serial.println(count);
+   } else {
+    //Serial.println("Não há sinal");
+   }
+   Serial.println(flowRate);         //Imprime a variável flowRate no Serial
+    
   float umidade = analogRead(recebeUmidade);  
   // Firebase.setString(bd, "/escola/canteiro1/umidade", umidade);
   
@@ -73,29 +105,11 @@ void loop() {
   // padrao entre 4050 X 2600
 
   //Varifica automatico se estiver libera o controle de liberar ou não água
-  if (Firebase.get(bd, "/escola/canteiro1/automatico")) {
-    if (bd.dataType() == "string") {
-      String automatico = bd.stringData();
-      if (automatico == "1") {
-
-        if (umidade < 3600) {
-
-          Firebase.setString(bd, "/escola/canteiro1/isOpen", "1");
-          // Firebase.pushString(bd, "/canteiro2/historicoUmidade", umidade);// maoir de 4000
-
-        } else {
-
-          // Firebase.pushString(bd, "/canteiro2/historicoUmidade", umidade);//
-          Firebase.setString(bd, "/escola/canteiro1/isOpen", "0");
-        }
-        
-      }
-    }
-  }
+  //Este sistema se ativo, desabilita o controle remoto manual
+  //Revisar o código dessa função antes de ativar.
+  canteiroAutomatico(false, umidade);
 
   if (Firebase.get(bd, "/escola/canteiro1/isOpen")) {
-    caminhoUmidade = "/escola/canteiro1/umidade/"+obterData();
-     Firebase.setString(bd, caminhoUmidade, umidade);
     if (bd.dataType() == "string") {
       String RL1 = bd.stringData();
       if (RL1 == "0") {
@@ -107,6 +121,14 @@ void loop() {
         // as ações
         Firebase.setString(bd, "/escola/canteiro1/status", "Aberto");
         digitalWrite(canteiro1, LOW);
+
+        //Escreve a umidade na base de dados apenas se o canteiro estiver Aberto
+        caminhoUmidade = "/escola/canteiro1/umidade/"+obterData();
+        Firebase.setString(bd, caminhoUmidade, umidade);
+
+        //Escreve o fluxo de agua na base de dados apenas se o canteiro estiver Aberto
+        caminhoFluxo = "/escola/canteiro1/fluxo/"+obterData();
+        Firebase.setString(bd, caminhoFluxo, flowRate);
       }
       
     }
@@ -179,6 +201,38 @@ void conectarWifi()
   Serial.println("Wifi Conectado!!!");
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
+
+void canteiroAutomatico(bool ativo, float umidade)
+{
+  if(!ativo)
+  {
+    return;
+  }
+  
+  if (Firebase.get(bd, "/escola/canteiro1/automatico")) {
+    if (bd.dataType() == "string") {
+      String automatico = bd.stringData();
+      if (automatico == "1") {
+
+        if (umidade < 3600) {
+
+          Firebase.setString(bd, "/escola/canteiro1/isOpen", "1");
+          // Firebase.pushString(bd, "/canteiro2/historicoUmidade", umidade);// maoir de 4000
+
+        } else {
+
+          // Firebase.pushString(bd, "/canteiro2/historicoUmidade", umidade);//
+          Firebase.setString(bd, "/escola/canteiro1/isOpen", "0");
+        }
+        
+      }
+    }
+  }
+}
+
+
+
+
 String obterData()
 {
   struct tm timeinfo;
@@ -206,7 +260,7 @@ String obterData()
   dia = formatarData(dia);
   //Formatando Hora
   String hora = String(timeinfo.tm_hour);
-  dia = formatarData(hora);
+  hora = formatarData(hora);
   //Formatando Min
   String minuto = String(timeinfo.tm_min);
   minuto = formatarData(minuto);
@@ -235,4 +289,9 @@ String formatarData(String date)
 void timeavailable(struct timeval *t)
 {
   Serial.println("Tem ajuste de tempo do NTP!");
+}
+
+void Flow()
+{
+   count++; //Quando essa função é chamada, soma-se 1 a variável "count" 
 }
